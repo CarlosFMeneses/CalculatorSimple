459b02c9f03076fc0f422b9df03847906ea67391
CarlosFMeneses <carlos.f.meneses@gmail.com>
2015-04-19 14:06:04 -0400

    Initial Commit. Only GUI, no event listeners

---
+++ CalculatorSimple.java
@@ -0,0 +1,77 @@
+/*
+Carlos F. Meneses
+04/14/2015
+Bergen Community College
+Advanced Java Programming
+*/
+/**
+CalculatorSimple.java: Creates a basic calculator using the Java GUI component 
+GridLayout. App performs basic four function (add, subtract, muultiply, and 
+divide) on two integers and display the result. GUI bases on the keyboard layout 
+of the Busicom LE-120A "HANDY-LE", except "=" has been ommited, since this app 
+only excepts 2 integers.
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorSimple extends JFrame
+{
+	// define buttons
+	private JButton bZero = new JButton("0");
+	private JButton bOne = new JButton("1");
+	private JButton bTwo = new JButton("2");
+	private JButton bThree = new JButton("3");
+	private JButton bFour = new JButton("4");
+	private JButton bFive = new JButton("5");
+	private JButton bSix = new JButton("6");
+	private JButton bSeven = new JButton("7");
+	private JButton bEight = new JButton("8");
+	private JButton bNine = new JButton("9");
+	private JButton bAdd = new JButton("+");
+	private JButton bSubtract = new JButton("-");
+	private JButton bMultiply = new JButton("X");
+	private JButton bDivide = new JButton("÷");
+	private JButton bDecimal = new JButton(".");
+
+	private GridLayout layout = new GridLayout(4,4,3,3);
+	private Container con = getContentPane();
+
+	public CalculatorSimple()
+	{
+		super("Simple Calculator");
+
+		con.setLayout(layout);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		con.add(bSeven);
+		con.add(bEight);
+		con.add(bNine);
+		con.add(bMultiply);
+
+		con.add(bFour);
+		con.add(bFive);
+		con.add(bSix);
+		con.add(bDivide);
+
+		con.add(bOne);
+		con.add(bTwo);
+		con.add(bThree);
+		con.add(bSubtract);
+
+		con.add(bZero);
+		con.add(new JLabel());
+		con.add(bDecimal);
+		con.add(bAdd);
+		con.add(bAdd);
+
+		setSize(250,150);
+		setResizable(false);
+	}
+
+	public static void main(String[] args) 
+	{
+		CalculatorSimple frame = new CalculatorSimple();
+		frame.setVisible(true);
+	}
+}
